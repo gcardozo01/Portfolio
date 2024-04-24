f6154a83a6a6d87465af397cf7a3ce6279072cba
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2024-04-23 22:35:56 -0300

    componentization of about me section

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -5,12 +5,18 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [HeaderComponent, ProfileComponent, ExperiencesComponent],
+  imports: [
+    HeaderComponent,
+    ProfileComponent,
+    ExperiencesComponent,
+    AboutComponent,
+  ],
 })
 export class HomeComponent {}
