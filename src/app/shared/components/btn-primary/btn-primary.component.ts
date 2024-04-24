69a0c32d31d0fdf9f6ee484736994c7255e0da33
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2024-04-23 22:00:46 -0300

    adding btn-primary component

---
+++ src/app/shared/components/btn-primary/btn-primary.component.ts
@@ -0,0 +1,23 @@
+// Angular
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+type BtnVariants = 'primary' | 'secondary';
+@Component({
+  selector: 'btn-primary',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-primary.component.html',
+  styleUrl: './btn-primary.component.scss',
+})
+export class BtnPrimaryComponent {
+  @Input('btn-text') btnText: string = '';
+  @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
+  @Input() variant: BtnVariants = 'primary';
+
+  @Output('submit') onSubmit = new EventEmitter();
+
+  submit() {
+    this.onSubmit.emit();
+  }
+}
